261248101256a1e6414fb25a6a5ec4db777187af
Zakaria beta <zakariabeta001@gmail.com>
2021-01-26 17:59:26 +0100

    style

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,47 @@
-// (function ($) {
-//   app = {
-//     init: function () {
-//       this._slider();
-//     },
-//     _slider: function () {
-//       if ($(".slider-home__item").length > 1) {
-//         $(".slider-home").slick({
-//           infinite: false,
-//           dots: true,
-//           arrows: true,
-//           slidesToShow: 1,
-//           slidesToScroll: 1,
-//           // autoplay: true,
-//           autoplaySpeed: 2000,
-//         });
-//       }
-//     },
-//   };
+(function ($) {
+  app = {
+    init: function () {
+      app.Headroom();
+    },
+    Headroom: function () {
+      if ($('header').length > 0) {
+        var options = {
+          // vertical offset in px before element is first unpinned
+          offset: 0,
+          // scroll tolerance in px before state changes
+          tolerance: 5,
+          // css classes to apply
+          classes: {
+            // when element is initialised
+            initial: "headroom",
+            // when scrolling up
+            pinned: "is-sticky",
+            // when scrolling down
+            unpinned: "not-sticky",
+            // when above offset
+            top: "headroom--top",
+            // when below offset
+            notTop: "headroom--not-top",
+            // when at bottom of scoll area
+            bottom: "headroom--bottom",
+            // when not at bottom of scroll area
+            notBottom: "headroom--not-bottom"
+          }
+        }
+        // Element target
+        var myElement = document.querySelector("header");
+        var headroom = new Headroom(myElement);
+  
+        // initialise settings
+        headroom.offset = 0;//px
+  
+        headroom.init(options);
+      }
+    },
+  };
 
-//   app.init();
-// })(jQuery);
+  app.init();
+})(jQuery);
 $(document).ready(function() {
   AOS.init();
 })
